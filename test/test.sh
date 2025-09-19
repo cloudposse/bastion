1dbc94c201ec94a480b2b59ab2f15f77cdbb937f
Igor Rodionov <goruha@gmail.com>
2025-09-19 21:50:36 +0200

    Fix tests

--- test/test.sh
+++ test/test.sh
@@ -22,7 +22,7 @@ echo "Bastion sshd service started."
 
 docker compose exec bastion /scripts/setup.sh
 
-sleep 30
+sleep 120
 
 # greping for the first line of the left alignment square in the generated QR Code
 docker compose exec bastion /scripts/google_auth_qr_code_generator_test.sh |grep -F "[0m                                                                                          [0m" > /dev/null
